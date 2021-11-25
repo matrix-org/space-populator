b9ce7d16be45c75eb0a91c8e9e9e27e90cd63dbd
Thib <thibaultm@element.io>
2021-11-25 16:37:41 +0100

    Allow room creation when it doesn't already exist

--- permator.py
+++ permator.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description="Use Synapse admin API to join a us
 parser.add_argument("-u", "--user", help="Matrix ID of the user to add to the rooms and promote admin", required=True)
 parser.add_argument("-r", "--rooms", help="File containing the list of rooms to which the user should be added", required=True)
 parser.add_argument("-t", "--token", help="File containing the Acces Token of the Synapse administrator")
+parser.add_argument("-c", "--create", help="Create the room if it doesn't already exist", action="store_true")
 args = parser.parse_args()
 
 server_admin_access_token = None
@@ -29,7 +30,17 @@ with open(args.rooms, "r") as f:
     lines = f.readlines()
 
     for line in lines:
-        room = Room(line.strip())
+        alias = line.strip()
+        try:
+            room = Room(alias)
+        except ValueError:
+            if args.create:
+                Room.create(server_admin_access_token, alias)
+                room = Room(alias)
+            else:
+                logging.error("Room "+alias+" doesn't exist.")
+                print("Room "+alias+" doesn't exist.", file=sys.stderr)
+                break
         if room.visible_domain != args.user.strip().split(":")[1]:
             print("Room "+room.alias+" is on another server than "+args.user.strip()+", skipping", file=sys.stderr)
             logging.error("Room "+room.alias+" is on another server than "+args.user.strip()+", skipping")
